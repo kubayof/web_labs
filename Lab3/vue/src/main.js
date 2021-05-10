03019d79ef1c6c2c122d9290ad3a551b021967da
Oleh Kubai <kubayof82@gmail.com>
2021-05-11 01:10:13 +0300

    Report related stuff

--- Lab3/vue/src/main.js
+++ Lab3/vue/src/main.js
@@ -4,10 +4,9 @@ import App from './App.vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import router from './router'
-import store from './store'
 // import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-const app = createApp(App).use(store).use(router)
+const app = createApp(App).use(router)
 app.use(vuetify)
 
 app.mount('#app')
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## Web labs
-* [Lab1](/Lab1/README.md)
-* [Lab2](/Lab2/README.md)
+* [Lab1](/Lab1)
+* [Lab2](/Lab2)
+* [Lab3](/Lab3)
